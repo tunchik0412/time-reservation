ff496a7449f023348a4aba398326222729de9e51
Tuan Le <tunchik1997@gmail.com>
2024-07-23 18:09:38 +0300

    init

---
+++ frontend/vue-project/src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ frontend/vue-project/src/components/card/Card.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  active: Boolean,
+  src: String,
+  text: String
+})
+</script>
+
+<template>
+  <div :class="['card-wrapper', {active: props.active}]">
+    <div class="image-wrapper">
+      <div class="image-background">
+        <img :src="props.src" alt="Card Image" />
+      </div>
+    </div>
+    <p>{{props.text}}</p>
+  </div>
+</template>
+
+<style scoped type="text/scss">
+  .card-wrapper {
+    .image-wrapper {
+
+    }
+  }
+</style>
